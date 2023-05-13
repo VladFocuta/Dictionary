34fab20b7a270851bd975d4c96f61aadbc0e26db
Vlad Focuta <132595962+VladFocuta@users.noreply.github.com>
2023-05-13 18:19:14 +0200

    Update Dictionary.js

--- Dictionary.js
+++ Dictionary.js
@@ -12,20 +12,19 @@ function saveTheWord() {
    }
 }
 
-function searchForWords() {
+function wordExists() {
    let searchTerm = searchInput.value;
    let messageDiv = document.getElementById("message");
    if (words.length === 0 && searchTerm !== 0) {
       messageDiv.innerHTML = "There are no words in the dictionary yet";
    }
    if (searchTerm.length !== 0) {
-      for (let i = 0; i < words.length; ++i) {
-         if (searchTerm === words[i]) {
-            messageDiv.innerHTML = searchTerm + " " + "is in the dictionary";
-         } else if (searchTerm !== words[i]) {
-            messageDiv.innerHTML = searchTerm + " " + "is not in the dictionary";
-         }
+      if (words.includes(searchTerm)) {
+         messageDiv.innerHTML = searchTerm + " " + "is in the dictionary";
+      } else if (!words.includes(searchTerm)) {
+         messageDiv.innerHTML = searchTerm + " " + "is not in the dictionary";
       }
+
    } else if (searchTerm.length === 0) {
       messageDiv.innerHTML = "You need to type a word first";
    }
